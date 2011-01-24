d21f7e01195c735352294bd1b54185455c1a7960
Kyle Campbell <kmcampott@users.sourceforge.net>
2011-01-24 01:12:34 +0000

    0.23.1 work continued
    - Add missing files
    
    git-svn-id: https://opencachemanage.svn.sourceforge.net/svnroot/opencachemanage@185 7d34a30a-a1a5-49b2-a90b-01e4541fe654

---
+++ OpenCacheManager/ocm-gtk/LocationList.cs
@@ -0,0 +1,137 @@
+// 
+//  Copyright 2011  Kyle Campbell
+// 
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
+// 
+//        http://www.apache.org/licenses/LICENSE-2.0
+// 
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+
+using System;
+using System.IO;
+using System.Collections.Generic;
+using Gtk;
+using Mono.Unix;
+using System.Runtime.Serialization.Formatters.Binary;
+
+
+namespace ocmgtk
+{
+
+	[Serializable]
+	public class LocationList
+	{
+
+		public LocationList ()
+		{
+		}
+		
+		private Dictionary<string, Location> m_locations = new Dictionary<string, Location>();
+		
+		public Location[] Locations
+		{
+			get{
+				Location[] locs = new Location[m_locations.Count];
+				m_locations.Values.CopyTo(locs, 0);
+				return locs;
+			}
+			set{
+				foreach(Location location in value)
+				{
+					m_locations.Add(location.Name, location);
+				}				
+			}
+		}
+
+		
+		public void AddLocation(Location loc)
+		{
+			if (m_locations.ContainsKey(loc.Name))
+			{
+				MessageDialog dlg = new MessageDialog(null, DialogFlags.Modal, MessageType.Question, ButtonsType.YesNo,
+				                                      String.Format(Catalog.GetString("Are you sure you wish to " +
+				                                      	"overwrite \"{0}\"?"), loc.Name));
+				if ((int) ResponseType.Yes != dlg.Run())
+				{
+					dlg.Hide();
+					return;
+				}
+				else
+				{
+					dlg.Hide();
+					m_locations.Remove(loc.Name);
+				}
+			}
+			m_locations.Add(loc.Name, loc);
+			UpdateLocFile();
+		}
+		
+		public void DeleteLocation(string name)
+		{
+			m_locations.Remove(name);
+			UpdateLocFile();
+		}
+		
+		public static LocationList LoadLocationList()
+		{
+			String path = System.Environment.GetFolderPath (System.Environment.SpecialFolder.ApplicationData);
+			if (!File.Exists(path + "/ocm/locs.oqf"))
+			{
+				return new LocationList();
+			}
+			FileStream fs = new FileStream(path + "/ocm/locs.oqf", FileMode.Open, FileAccess.Read);	
+			BinaryFormatter ser = new BinaryFormatter();
+			System.Object filters = ser.Deserialize(fs);
+			fs.Close();
+			return filters as LocationList;
+		}
+		
+		private void UpdateLocFile()
+		{
+			String path = System.Environment.GetFolderPath (System.Environment.SpecialFolder.ApplicationData);
+			if (!Directory.Exists("ocm"))
+				Directory.CreateDirectory(path + "/ocm");
+			path = path + "/ocm";
+			BinaryFormatter ser = new BinaryFormatter();
+			FileStream fs = new FileStream(path + "/locs.oqf", FileMode.Create, FileAccess.ReadWrite);
+			ser.Serialize(fs, this);
+			fs.Close();
+		}
+		
+		public Menu BuildLocationlMenu()
+		{
+			Menu etMenu = new Menu();
+			
+			Gtk.Action home_action = new Gtk.Action("Home", Catalog.GetString("Home"));
+			etMenu.Append(home_action.CreateMenuItem());
+			home_action.Activated += HandleHome_actionActivated;
+			int iCount = 0;
+			foreach (Location loc in m_locations.Values)
+			{
+				Gtk.Action action = new Gtk.Action(loc.Name, loc.Name);
+				etMenu.Append(action.CreateMenuItem());
+				action.Activated += HandleActionActivated;
+				iCount ++;
+				
+			}
+			return etMenu;
+		}
+
+		void HandleHome_actionActivated (object sender, EventArgs e)
+		{
+			UIMonitor.getInstance().ResetCenterToHome();
+		}
+		
+		void HandleActionActivated (object sender, EventArgs e)
+		{
+			Location loc = m_locations[((sender) as Gtk.Action).Name];
+			UIMonitor.getInstance().SetLocation(loc);
+		}
+	}
+}
